d62ef9345c9861262b3c7a515fdede84042ba111
Eckart Schneider <eckshn@gmail.com>
2020-12-04 15:43:01 -0500

    try

--- src/main/java/com/team4099/falconcamps/Robot.java
+++ src/main/java/com/team4099/falconcamps/Robot.java
@@ -1,12 +1,44 @@
 package com.team4099.falconcamps;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
     // These fields have been created for you, but you'll need to add an assignment
-    private TalonSRX intake;
-    private CANSparkMax shooter;
-    private CANSparkMax shooterFollower;
+    private TalonSRX intake = new TalonSRX(5);
+    private CANSparkMax shooter = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    @Override public void robotInit(){
+        intake.set(ControlMode.PercentOutput, 0.5);
+        shooter.set(0.5);
+        shooterFollower.follow(shooter, true);
+    }
+
+    @Override public void autonomousInit(){
+
+    }
+
+    @Override public void teleopInit(){
+
+    }
+
+    @Override public void disabledInit(){
+
+    }
+
+    @Override public void robotPeriodic(){
+
+    }
+
+    @Override public void autonomousPeriodic(){
+
+    }
+
+    @Override public void disabledPeriodic(){
+
+    }
 }
